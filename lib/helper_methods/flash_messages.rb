47f3ba735c66b67aef26cf466d27d902c7d52fc1
Francisco Martins <franciscomxs@gmail.com>
2014-09-27 21:23:49 -0300

    Improve flash and error messages

--- lib/helper_methods/flash_messages.rb
+++ lib/helper_methods/flash_messages.rb
@@ -1,8 +1,8 @@
 module HelperMethods
 
-  def error_messages_for(resource, style = 'danger')
+  def error_messages_for(resource:, style: 'danger')
     if resource.errors.any?
-      content_tag :div, class: "alert alert-#{style} alert-dismissable" do
+      content_tag :div, class: "alert alert-#{styles[style]} alert-dismissible" do
         content_tag :ol do
           resource.errors.collect do |key, value|
             content_tag :li, value
@@ -13,17 +13,28 @@ module HelperMethods
   end
 
   def flash_messages
-    if flash.any?
-      content_tag :div, class: "alert alert-success alert-dismissable" do
-        content_tag :ul do
-          flash.collect do |key, value|
-            unless [true, false, nil].include?(value)
-              content_tag :li, value
-            end
-          end.join.html_safe
-        end
-      end
+    flash.collect do |key, value|
+      content_tag :div, class: "alert alert-#{styles[key]} alert-dismissable", role: "alert" do
+        [close_button, value].join.html_safe
+      end unless [true, false, nil].include?(value)
+    end.join.html_safe
+  end
+
+  def close_button
+    content_tag :button, type: 'button', class: 'close', data: { dismiss: 'alert' } do
+      content_tag(:span, '×', aria: { hidden: 'true' }) + content_tag(:span, 'Close', class: 'sr-only')
     end
+  end
+
+  def styles
+    {
+      'notice' => 'success',
+      'alert' => 'warning',
+      'success' => 'success',
+      'info' => 'info',
+      'warning' => 'warning',
+      'danger' => 'danger'
+    }
   end
 
 end
--- lib/helper_methods/version.rb
+++ lib/helper_methods/version.rb
@@ -1,3 +1,3 @@
 module HelperMethods
-  VERSION = '1.0.0.rc1'
+  VERSION = '1.0.0.rc2'
 end
